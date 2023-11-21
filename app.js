81acd24d10734203f186f209551a401f55f56bd1
Ahmedrauf1234 <Ahmedrauf261@gmail.com>
2023-11-21 15:35:47 +0500

    completed

--- app.js
+++ app.js
@@ -1,42 +1,29 @@
 const input = document.querySelector("#inp")
-const ul = document.querySelector("#ul")
+let ul = document.querySelector("#ul")
 let array = []
 function renderTodo(){
     for( let i = 0; i < array.length; i++ ){
         ul.innerHTML += `<li class = "list" >  ${array[i]} <button class="btn"  onclick="deleteTodo(${i})" > Delete </button> <button class="btn" onclick="EditTodo(${i})">Edit</button>  </li>`
-       
     }
 }
-
-
-
-
-
 //add work start
 function add(){
     
    if(input.value == ""){
-    alert("please Enter Value")
+    alert("please Enter Text")
    }
    else{
     ul.innerHTML = ""
     array.push(input.value)
     console.log(array);
     renderTodo()
-    
-   }
-   input.value = ""
-        
+    input.value = ""
 }
-   
-    
-
+}
 //buttons work (delete All)
 function Deleteall(){
-    input.value = ""
     ul.innerHTML = ""
-    
-    
+    array = []
 }
 //end
 
@@ -46,25 +33,19 @@ function Deleteall(){
     array.splice(i,1)
     console.log(array);
     renderTodo()
-
- }
-
-
+}
+//end
 //editTodo work
 function EditTodo(i){
-   
     let User = prompt("Enter Text?")
     if(User === ""){
-        alert("Enter Value")
+        alert("please Enter Text")
     }
     else{
         ul.innerHTML = ""
         array.splice(i,1,User)
         renderTodo()
-
     }
-   
-
 } 
 
 
